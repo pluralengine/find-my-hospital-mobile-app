9051d370dbc48a8a4653acc8665f3cea2669cc30
Ivan Ortega <iortega@newrelic.com>
2020-04-12 20:21:18 +0200

    feat: color markers depending on hospital status

---
+++ styles/palette.ts
@@ -0,0 +1,19 @@
+export const STATUS_PALETTE = [
+  "#cc3232",
+  "#db7b2b",
+  "#e7b416",
+  "#99c140",
+  "#2dc937",
+];
+
+export const STATUS_EMOJIS = [
+  "weary",
+  "slightly_frowning_face",
+  "neutral_face",
+  "slightly_smiling_face",
+  "grin",
+];
+
+export const STATUS_MARKER_PALETTE = [
+    "red", "orange", "gold", "yellow", "green"
+]
