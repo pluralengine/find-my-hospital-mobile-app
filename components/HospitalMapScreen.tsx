c7a456d2cf53b72aa70ec70741123dc6fe9de754
Ivan Ortega <iortega@newrelic.com>
2020-04-17 00:47:42 +0200

    feat: adapt map to pharmacies

---
+++ components/HospitalMapScreen.tsx
@@ -0,0 +1,107 @@
+import "react-native-gesture-handler";
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  Text,
+} from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import { getHospitals } from "../api";
+import useLogin from "../hooks/useLogin";
+import VoteBar from "./VoteBar";
+import { STATUS_PALETTE, STATUS_MARKER_PALETTE } from "../styles/palette";
+
+export default function HospitalMapScreen({ navigation }) {
+  const [hospitals, setHospitals] = useState([]);
+  const { user, logout } = useLogin();
+  const isLoggedIn = Boolean(user && user.email);
+  const showVoteBar = isLoggedIn;
+
+  useEffect(() => {
+    getHospitals().then((hospitals) => setHospitals(hospitals));
+  }, []);
+
+  function renderLogin() {
+    return isLoggedIn ? (
+      <TouchableOpacity style={styles.loginButton} onPress={logout}>
+        <Text>Cerrar sesión</Text>
+      </TouchableOpacity>
+    ) : (
+      <TouchableOpacity
+        style={styles.loginButton}
+        onPress={() => navigation.navigate("Login")}
+      >
+        <Text>¿Eres personal sanitario?</Text>
+      </TouchableOpacity>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={[styles.map, { height: showVoteBar ? "80%" : "100%" }]}
+        provider={"google"}
+        showsUserLocation
+        showsMyLocationButton
+        showsCompass
+      >
+        {hospitals.map((hospital) => (
+          <Marker
+            key={hospital.id}
+            tracksViewChanges={false}
+            coordinate={{
+              latitude: parseFloat(hospital.geometryLat),
+              longitude: parseFloat(hospital.geometryLng),
+            }}
+            title={hospital.name}
+            description={hospital.address}
+            pinColor={getPinColor(hospital.status)}
+          />
+        ))}
+      </MapView>
+      {showVoteBar && <VoteBar style={styles.bottomBar} />}
+      {renderLogin()}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    backgroundColor: "#F5F5F5",
+  },
+  map: {
+    width: "100%",
+    height: "80%",
+  },
+  loginButton: {
+    position: "absolute",
+    backgroundColor: "white",
+    borderRadius: 10,
+    borderColor: "#F5F5F5",
+    borderWidth: 1,
+    alignContent: "center",
+    justifyContent: "center",
+    textAlign: "center",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    right: 24,
+    top: 24,
+  },
+  bottomBar: {
+    height: "20%",
+    maxHeight: "20%",
+    overflow: "hidden",
+    width: "100%",
+  },
+});
+
+function getPinColor(status) {
+  if(!Number.isFinite(status)){
+    return 'linen'
+  }
+
+  return STATUS_MARKER_PALETTE[Math.round(status / 20) - 1];
+}
---
+++ components/utils.ts
@@ -0,0 +1,52 @@
+export function timeAgo(time) {
+  switch (typeof time) {
+    case "number":
+      break;
+    case "string":
+      time = +new Date(time);
+      break;
+    case "object":
+      if (time.constructor === Date) time = time.getTime();
+      break;
+    default:
+      time = +new Date();
+  }
+  const time_formats = [
+    [60, "segundos", 1], // 60
+    [120, "hace 1 minuto", "1 minuto"], // 60*2
+    [3600, "minutos", 60], // 60*60, 60
+    [7200, "1 hora", "1 hour hora"], // 60*60*2
+    [86400, "horas", 3600], // 60*60*24, 60*60
+    [172800, "ayer", "hoy"], // 60*60*24*2
+    [604800, "dias", 86400], // 60*60*24*7, 60*60*24
+    [1209600, "última semana", "próxima semana"], // 60*60*24*7*4*2
+    [2419200, "semanas", 604800], // 60*60*24*7*4, 60*60*24*7
+    [4838400, "último mes", "próximo mes"], // 60*60*24*7*4*2
+    [29030400, "meses", 2419200], // 60*60*24*7*4*12, 60*60*24*7*4
+    [58060800, "último año", "próximo year"], // 60*60*24*7*4*12*2
+    [2903040000, "años", 29030400], // 60*60*24*7*4*12*100, 60*60*24*7*4*12
+    [5806080000, "último centenario", "próximo centenario"], // 60*60*24*7*4*12*100*2
+    [58060800000, "centenarios", 2903040000], // 60*60*24*7*4*12*100*20, 60*60*24*7*4*12*100
+  ];
+  const seconds = (+new Date() - time) / 1000,
+    token = "hace ",
+    list_choice = 1;
+
+  if (seconds < 1) {
+    return "justo ahora";
+  }
+  if (seconds < 0) {
+    seconds = Math.abs(seconds);
+    token = "siguientes";
+    list_choice = 2;
+  }
+  let i = 0,
+    format;
+  while ((format = time_formats[i++]))
+    if (seconds < format[0]) {
+      if (typeof format[2] == "string") return format[list_choice];
+      else
+        return token + Math.floor(seconds / format[2]) + " " + format[1] + " ";
+    }
+  return time;
+}
