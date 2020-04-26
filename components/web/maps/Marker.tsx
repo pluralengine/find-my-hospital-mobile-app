889abbd32c7b662dcdb529e77e3b551c5d79e503
Marta <martacolombas@gmail.com>
2020-04-26 20:58:02 +0200

    fix: Markers is web are displayed now with the right color according to stock

--- components/web/maps/Marker.tsx
+++ components/web/maps/Marker.tsx
@@ -1,18 +1,27 @@
-import React, { Component } from "react";
-import { Marker } from "react-google-maps";
+import React, { Component } from 'react';
+import { Marker } from 'react-google-maps';
 
 class MapViewMarker extends Component {
   render() {
-    const { description, title, coordinate, children, onPress, ...rest } = this.props;
+    const {
+      description,
+      title,
+      coordinate,
+      children,
+      onPress,
+      pinColor,
+      ...rest
+    } = this.props;
+    let url = `http://maps.google.com/mapfiles/ms/icons/${pinColor}-dot.png`;
     return (
       <Marker
         {...rest}
         onClick={onPress}
         title={description ? `${title}\n${description}` : title}
         position={{ lat: coordinate.latitude, lng: coordinate.longitude }}
-      >
+        icon={url}>
         {children}
-        </Marker>
+      </Marker>
     );
   }
 }
